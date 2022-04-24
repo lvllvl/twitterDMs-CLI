565c9b7b27a1eb9dadcd4adb32fee7e3b31e1d7f
Christopher Navarro <chrisidxloop@gmail.com>
2022-04-24 13:25:41 -0400

    Added boilerplate code for showing user interface (UI) from crate documentation

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,43 @@
-use std::io; 
-use tui::{ backend::CrosstermBackend, Terminal }; 
+use std::{ io, thread, time::Duration }; 
+use tui::{ 
+    backend::CrosstermBackend, 
+    widgets::{ Widget, Block, Borders },
+    layout::{ Layout, Constraint, Direction },
+    Terminal }; 
+use crossterm::{
+    event::{ self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode },
+    execute,
+    terminal::{ disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen },
+};
 
 fn main() -> Result<(), io::Error> {
 
-    let stdout = io::stdout(); 
+    // set up terminal 
+    enable_raw_mode()?; // what does this do? 
+    let mut stdout = io::stdout();
+    execute!( stdout, EnterAlternateScreen, EnableMouseCapture )?; // does thi capture keyboard?
     let backend = CrosstermBackend::new( stdout ); 
-    let mut terminal = Terminal::new( backend )?; 
+    let mut terminal = Terminal::new( backend )?;
+
+    terminal.draw( |f| {
+        let size = f.size();
+        let block = Block::default()
+            .title( "Block" )
+            .borders( Borders::ALL ); 
+        f.render_widget( block, size ); 
+    })?;  // what does'?' do here?
+    
+
+    thread::sleep( Duration::from_millis( 5000 )); 
+
+    // Restore terminal
+    disable_raw_mode()?; 
+    execute!( 
+        terminal.backend_mut(),
+        LeaveAlternateScreen,
+        DisableMouseCapture
+        )?; 
+    terminal.show_cursor()?; 
 
     Ok( () )
 
